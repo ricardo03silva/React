67056e0693d4040d9d1a415560e4c39765d8d008
ricardo03silva <rnsilva@DEV48.intranet.digitalsign.pt>
2023-04-04 13:00:14 +0100

    Project Start

--- 01-Pratice Project/src/App.js
+++ 01-Pratice Project/src/App.js
@@ -3,11 +3,19 @@ import React from 'react';
 import AddUser from './Components/Users/AddUser';
 
 const App = () => {
-    return (
-        <div>
-            <AddUser />
-        </div>
-    );
+    return <div></div>;
 };
 
 export default App;
+
+{
+    /* 
+            Form
+              Username
+              Age
+              Button (submit)
+
+            Users List
+              Name + (years old)
+            */
+}
--- 01-Pratice Project/src/Components/Users/AddUser.js
+++ 01-Pratice Project/src/Components/Users/AddUser.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
 import styles from './AddUser.module.css';
 
-const AddUser = () => {};
+const AddUser = () => {
+    const [userName, setUserName] = useState('');
+    const [userAge, setUserAge] = useState('');
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <div>
+            <form onSubmit={submitHandler}>
+                <div className="">
+                    <label>Name</label>
+                    <input type="text" />
+                </div>
+                <div className="">
+                    <label>Amount</label>
+                    <input type="number" />
+                </div>
+                <button type="submit">Add Expense</button>
+            </form>
+        </div>
+    );
+};
 
 export default AddUser;
