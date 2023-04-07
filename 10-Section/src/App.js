ae7019e112a527221b72888b4c096be704fe21a9
ricardo03silva <rnsilva@DEV48.intranet.digitalsign.pt>
2023-04-07 12:36:41 +0100

    Section10 DONE: React Hooks

--- 10-Section/src/App.js
+++ 10-Section/src/App.js
@@ -1,37 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const ctx = useContext(AuthContext);
 
-    useEffect(() => {
-        const storedLoginInfo = localStorage.getItem('isLogged');
-        if (storedLoginInfo === '1') {
-            setIsLoggedIn(true);
-        }
-    }, []);
-
-    const loginHandler = (email, password) => {
-        localStorage.setItem('isLogged', '1');
-        setIsLoggedIn(true);
-    };
-
-    const logoutHandler = () => {
-        setIsLoggedIn(false);
-    };
-
-    return (
-        <React.Fragment>
-            <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-            <main>
-                {!isLoggedIn && <Login onLogin={loginHandler} />}
-                {isLoggedIn && <Home onLogout={logoutHandler} />}
-            </main>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <MainHeader />
+      <main>
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
