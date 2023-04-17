276650db5a88e784c1111956fb971b255f3d9ed4
ricardo03silva <rnsilva@DEV48.intranet.digitalsign.pt>
2023-04-17 12:27:16 +0100

    Section19 BEGIN

--- 19-Section/src/components/Shop/Products.jsx
+++ 19-Section/src/components/Shop/Products.jsx
@@ -1,19 +1,44 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const items = [
+    {
+        id: 1,
+        title: 'Product1',
+        price: 6,
+        description: 'Product Description',
+    },
+    {
+        id: 2,
+        title: 'Product2',
+        price: 2.45,
+        description: 'Product Description',
+    },
+    {
+        id: 3,
+        title: 'Product3',
+        price: 8.3,
+        description: 'Product Description',
+    },
+    {
+        id: 4,
+        title: 'Product4',
+        price: 6.3,
+        description: 'Product Description',
+    },
+];
+
 const Products = (props) => {
-  return (
-    <section className={classes.products}>
-      <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
-      </ul>
-    </section>
-  );
+    return (
+        <section className={classes.products}>
+            <h2>Buy your favorite products</h2>
+            <ul>
+                {items.map((el) => {
+                    return <ProductItem key={el.id} title={el.title} price={el.price} description={el.description} />;
+                })}
+            </ul>
+        </section>
+    );
 };
 
 export default Products;
